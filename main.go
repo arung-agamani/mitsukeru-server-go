f94bebd784233ffd2516fb6a188c5d56cbbe5b44
Arung Agamani <arung.agamani@gmail.com>
2024-05-10 13:48:27 +0700

    chore: mvp 2 with static serve

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/arung-agamani/mitsukeru-server-go/config"
 	"github.com/arung-agamani/mitsukeru-server-go/controllers"
 	"github.com/arung-agamani/mitsukeru-server-go/db"
@@ -34,6 +35,7 @@ import (
 	"github.com/arung-agamani/mitsukeru-server-go/utils/validator"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/signal"
@@ -51,38 +53,42 @@ func main() {
 
 	deps := services.NewDependencies()
 	router := mux.NewRouter()
-	router.HandleFunc("/", controllers.HelloHandler()).Methods(http.MethodGet)
-	router.HandleFunc("/info", controllers.InfoHandler()).Methods(http.MethodGet)
-	router.HandleFunc("/lost-item", controllers.CreateLostItemHandler(deps)).Methods(http.MethodPost)
-	router.HandleFunc("/lost-item", controllers.ListLostItemHandler(deps)).Methods(http.MethodGet)
-	router.HandleFunc("/lost-items/{itemId}", controllers.GetLostItemHandler(deps)).Methods(http.MethodGet)
-	router.HandleFunc("/lost-items/{itemId}", controllers.UpdateLostItemHandler(deps)).Methods(http.MethodPost)
-	router.HandleFunc("/lost-items/{itemId}", controllers.DeleteLostItemHandler(deps)).Methods(http.MethodDelete)
+	//router.HandleFunc("/", controllers.HelloHandler()).Methods(http.MethodGet)
+	router.HandleFunc("/api/info", controllers.InfoHandler()).Methods(http.MethodGet)
+	router.HandleFunc("/api/lost-item", controllers.CreateLostItemHandler(deps)).Methods(http.MethodPost)
+	router.HandleFunc("/api/lost-item", controllers.ListLostItemHandler(deps)).Methods(http.MethodGet)
+	router.HandleFunc("/api/lost-items/{itemId}", controllers.GetLostItemHandler(deps)).Methods(http.MethodGet)
+	router.HandleFunc("/api/lost-items/{itemId}", controllers.UpdateLostItemHandler(deps)).Methods(http.MethodPost)
+	router.HandleFunc("/api/lost-items/{itemId}", controllers.DeleteLostItemHandler(deps)).Methods(http.MethodDelete)
 
-	router.HandleFunc("/auth/login", controllers.LoginHandler(deps)).Methods(http.MethodPost)
-	router.HandleFunc("/auth/signup", controllers.SignInHandler(deps)).Methods(http.MethodPost)
-	router.HandleFunc("/auth/logout", controllers.LogoutHandler()).Methods(http.MethodPost)
+	router.HandleFunc("/api/auth/login", controllers.LoginHandler(deps)).Methods(http.MethodPost)
+	router.HandleFunc("/api/auth/signup", controllers.SignInHandler(deps)).Methods(http.MethodPost)
+	router.HandleFunc("/api/auth/logout", controllers.LogoutHandler()).Methods(http.MethodPost)
 
-	router.HandleFunc("/item-type", controllers.ListItemType(deps)).Methods(http.MethodGet)
-	router.HandleFunc("/item-type", controllers.CreateItemType(deps)).Methods(http.MethodPost)
-	router.HandleFunc("/item-types/{name}", controllers.GetItemType(deps)).Methods(http.MethodGet)
-	router.HandleFunc("/item-types/{name}", controllers.UpdateItemType(deps)).Methods(http.MethodPost)
-	router.HandleFunc("/item-types/{name}", controllers.DeleteItemType(deps)).Methods(http.MethodDelete)
+	router.HandleFunc("/api/item-type", controllers.ListItemType(deps)).Methods(http.MethodGet)
+	router.HandleFunc("/api/item-type", controllers.CreateItemType(deps)).Methods(http.MethodPost)
+	router.HandleFunc("/api/item-types/{name}", controllers.GetItemType(deps)).Methods(http.MethodGet)
+	router.HandleFunc("/api/item-types/{name}", controllers.UpdateItemType(deps)).Methods(http.MethodPost)
+	router.HandleFunc("/api/item-types/{name}", controllers.DeleteItemType(deps)).Methods(http.MethodDelete)
 
-	router.HandleFunc("/event", controllers.CreateEventHandler()).Methods(http.MethodPost)
-	router.HandleFunc("/event", controllers.ListEventHandler(deps)).Methods(http.MethodGet)
-	router.HandleFunc("/events/{eventId}", controllers.DeleteEventHandler()).Methods(http.MethodDelete)
-	router.HandleFunc("/events/{eventId}", controllers.GetEventHandler()).Methods(http.MethodGet)
-	router.HandleFunc("/events/{eventId}", controllers.UpdateEventHandler()).Methods(http.MethodPost)
+	router.HandleFunc("/api/event", controllers.CreateEventHandler()).Methods(http.MethodPost)
+	router.HandleFunc("/api/event", controllers.ListEventHandler(deps)).Methods(http.MethodGet)
+	router.HandleFunc("/api/events/{eventId}", controllers.DeleteEventHandler()).Methods(http.MethodDelete)
+	router.HandleFunc("/api/events/{eventId}", controllers.GetEventHandler()).Methods(http.MethodGet)
+	router.HandleFunc("/api/events/{eventId}", controllers.UpdateEventHandler()).Methods(http.MethodPost)
 
-	router.HandleFunc("/user", controllers.CreateUserHandler(deps)).Methods(http.MethodPost)
-	router.HandleFunc("/users/{userId}", controllers.GetUserHandler(deps)).Methods(http.MethodGet)
+	router.HandleFunc("/api/user", controllers.CreateUserHandler(deps)).Methods(http.MethodPost)
+	router.HandleFunc("/api/users/{userId}", controllers.GetUserHandler(deps)).Methods(http.MethodGet)
 
+	//router.PathPrefix("/").Handler(http.FileServer(http.Dir("./dist")))
+	router.PathPrefix("/assets").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./dist/assets"))))
+	router.HandleFunc("/", Index()).Methods(http.MethodGet)
 	//router.HandleFunc("/event/{eventId}")
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{
 			"http://localhost:14045",
 			"http://localhost:5173",
+			"https://cf.howlingmoon.dev",
 		},
 		AllowCredentials: true,
 		Debug:            true,
@@ -102,4 +108,12 @@ func main() {
 	logger.Info("Shutting down server...")
 	server.Shutdown(context.Background())
 
+}
+
+func Index() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		index, _ := ioutil.ReadFile(fmt.Sprintf("%s/%s", "dist", "index.html"))
+		_, _ = w.Write(index)
+		return
+	}
 }
